a700f2fe34f681ac0acaeea9a6359eafbd0ff921
Vegadrone <spenaantonino@gmail.com>
2022-06-13 15:37:18 +0200

    Add User favorite color data acquisition + console.log

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,16 @@
 
 const userName = prompt ('Inserisci il tuo nome')
 document.getElementById ('user-name').innerHTML = userName
+console.log (userName)
 
 /* USER LAST NAME */
 
 const userLastName = prompt('Inserisci il tuo cognome')
 document.getElementById ('user-last-name').innerHTML = userLastName
- 
+console.log (userLastName)
+
+/* USER FAVORITE COLOR */
+
+const userFavColor = prompt ('Inserisci il tuo colore preferito')
+document.getElementById ('user-favorite-color').innerHTML = userFavColor
+console.log (userFavColor)
