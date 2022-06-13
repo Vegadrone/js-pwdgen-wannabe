d090ca570d2e192d0f39e0b25d25716fd8dacfc4
Vegadrone <spenaantonino@gmail.com>
2022-06-13 15:27:55 +0200

    Add in JS Last Name data acquisition.

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,9 @@
 
 const userName = prompt ('Inserisci il tuo nome')
 document.getElementById ('user-name').innerHTML = userName
+
+/* USER LAST NAME */
+
+const userLastName = prompt('Inserisci il tuo cognome')
+document.getElementById ('user-last-name').innerHTML = userLastName
+ 
