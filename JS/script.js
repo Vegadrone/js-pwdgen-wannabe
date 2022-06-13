bbe3eca9c02b12ba775fc0b5c355d9784a142ea5
Vegadrone <spenaantonino@gmail.com>
2022-06-13 16:27:05 +0200

    Change  p tag in span and add comments in JS

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,35 @@
 /* USER NAME */
+/* Creo una variabile il cui valore è inserito
+direttamente dall'utente attraverso prompt*/
 
 const userName = prompt ('Inserisci il tuo nome');
 document.getElementById ('user-name').innerHTML = userName;
 console.log (userName);
 
 /* USER LAST NAME */
+/* Creo una variabile il cui valore è inserito
+direttamente dall'utente attraverso prompt.
+Lo aggiungo tramite document.getElementById ('id').innerHTML 
+all'html*/
 
 const userLastName = prompt('Inserisci il tuo cognome');
 document.getElementById ('user-last-name').innerHTML = userLastName;
 console.log (userLastName);
 
 /* USER FAVORITE COLOR */
-
+/* Creo una variabile il cui valore è inserito
+direttamente dall'utente attraverso prompt
+Lo aggiungo tramite document.getElementById ('id').innerHTML 
+all'html*/
 const userFavColor = prompt ('Inserisci il tuo colore preferito');
 document.getElementById ('user-favorite-color').innerHTML = userFavColor;
 console.log (userFavColor);
 
 /* USER NEW PASSWORD */
+/* Metto insieme tutte le variabili generate dall'inserimento
+dei dati da parte dell'utente e creo una password aggiungendo 
+un numero (22).Lo aggiungo tramite document.getElementById ('id').innerHTML 
+all'html*/
 
 const userNewPassword = userName + userLastName + userFavColor + 22;
 document.getElementById ('user-new-password').innerHTML = userNewPassword
