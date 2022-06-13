2b4c408cc30bf8175abe582fb7efc9e67fcae72d
Vegadrone <spenaantonino@gmail.com>
2022-06-13 16:20:03 +0200

    Add User new password generation. Exercise complete.

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,23 @@
 /* USER NAME */
 
-const userName = prompt ('Inserisci il tuo nome')
-document.getElementById ('user-name').innerHTML = userName
-console.log (userName)
+const userName = prompt ('Inserisci il tuo nome');
+document.getElementById ('user-name').innerHTML = userName;
+console.log (userName);
 
 /* USER LAST NAME */
 
-const userLastName = prompt('Inserisci il tuo cognome')
-document.getElementById ('user-last-name').innerHTML = userLastName
-console.log (userLastName)
+const userLastName = prompt('Inserisci il tuo cognome');
+document.getElementById ('user-last-name').innerHTML = userLastName;
+console.log (userLastName);
 
 /* USER FAVORITE COLOR */
 
-const userFavColor = prompt ('Inserisci il tuo colore preferito')
-document.getElementById ('user-favorite-color').innerHTML = userFavColor
-console.log (userFavColor)
+const userFavColor = prompt ('Inserisci il tuo colore preferito');
+document.getElementById ('user-favorite-color').innerHTML = userFavColor;
+console.log (userFavColor);
+
+/* USER NEW PASSWORD */
+
+const userNewPassword = userName + userLastName + userFavColor + 22;
+document.getElementById ('user-new-password').innerHTML = userNewPassword
+console.log(userNewPassword);
